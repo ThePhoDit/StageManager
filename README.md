4338bab9a3f3ff9dab05def2d60c95c06e28c1d9
Mario <mario.vitoon@gmail.com>
2021-06-15 18:30:56 +0200

    Readme and comments

---
+++ README.md
@@ -0,0 +1,18 @@
+# Stage Manager for Discord
+
+Easily manage who speaks next in your stage channel by the order members raised their hands. You can also add priority roles (every three priority speakers a regular one is picked).
+
+## Commands
+
+- `v!start` when no audience member is speaking.
+- `v!next` when an audience member is speaking.
+
+## How to setup?
+
+1. Clone the repo.
+2. Create a `.env` file and add your bot's token.
+```env
+TOKEN=YOUR_TOKEN_GOES_HERE
+```
+3. Edit the fields __prefix__ and __priorityRoles__ from the `index.js` file as stated there.
+4. Run the file `node .`.
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const { Client } = require('discord.js');
 require('dotenv').config();
 
+// In the following list add any roles (IDs) if you want members with it to have priority. The first roles will have the highes priority, and the final ones, the lowest.
 priorityRoles = [];
-timesPrioritized = 0;
-prefix = 'v!'
-previousUser = ''
+timesPrioritized = 0; // DO NOT CHANGE
+prefix = 'v!' // The prefix you want to use for your bot.
+previousUser = '' // DO NOT CHANGE
 
 async function nextSpeakerByOrder(members) {
 	const member = await members.filter(m => m.voice.requestToSpeakTimestamp && m.voice.suppress).sort((m1, m2) => m1.voice.requestToSpeakTimestamp - m2.voice.requestToSpeakTimestamp).first();
@@ -47,11 +48,13 @@ client.on('message', async (msg) => {
 	if (!msg.member.permissions.has('MOVE_MEMBERS')) return;
 	if (!msg.member.voice.channelID || msg.member.voice.channel.type !== 'stage')
 		return msg.channel.send('You are not on a stage channel.');
+
 	if (!msg.content.startsWith(prefix)) return;
 	const args = msg.content.trim().split(/ +/g);
 	if (args[0] === prefix) return;
 	const command = args.shift().slice(prefix.length);
 	if (!command) return;
+
 	if (command === 'start') {
 		const member = await nextSpeaker(msg.member.voice.channel.members);
 		if (!member)
@@ -63,7 +66,9 @@ client.on('message', async (msg) => {
 	}
 
 	if (command === 'next') {
+		// Remove currently speaking user.
 		(await msg.guild.members.fetch(previousUser)).voice.setSuppressed(true);
+
 		const member = await nextSpeaker(msg.member.voice.channel.members);
 		if (!member)
 			return msg.channel.send('No users with hands risen.');
